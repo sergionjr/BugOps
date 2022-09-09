ea7f8c4bff884d89dc5889a1da28fb2969e17076
sergionjr <seno7509@colorado.edu>
2022-09-08 20:58:24 -0600

    index script testing

--- BugOps/BugOps/views.py
+++ BugOps/BugOps/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("You have arrived on the index for the BugOps proj.")
+    return HttpResponse("<h1>You have arrived on the index for the BugOps proj.<h1>")
