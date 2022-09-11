11e4a1988ca64842b79ec73402330fac8c232f9d
sergionjr <seno7509@colorado.edu>
2022-09-10 23:24:13 -0600

    rendering pages with views.py

--- BugOps/views.py
+++ BugOps/views.py
@@ -1,4 +1,14 @@
+from curses.ascii import HT
 from django.http import HttpResponse
+from django.shortcuts import render
+
+# from . import
+
+def home(request):
+    return HttpResponse("<h1>Arrived on the home page for BugOps!<h1>")
 
 def index(request):
     return HttpResponse("<h1>You have arrived on the index for the BugOps proj.<h1>")
+
+def login(request):
+    return render(request, 'templates/registration/login.html')
