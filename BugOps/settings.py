1c4319d6508a9d17f70f652067dfd66e4d4d7c75
sergionjr <seno7509@colorado.edu>
2022-09-16 18:34:12 -0600

    PLEASE WORK

--- BugOps/settings.py
+++ BugOps/settings.py
@@ -119,7 +119,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, )
+]
 print("ASDSADASDASDSDADSDSSA", BASE_DIR)
+print("BXBXBXBXBXBXBXBXBX", BASE_DIR)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
